a05b23ee0c8a499ec2cafa5ddcf76460c78147b4
CHARANCHERRY123456 <cherryiiit1234@gmail.com>
2026-02-23 10:06:41 +0530

    Update CORS middleware to allow requests from localhost on port 8080

--- govproject-backend/main.py
+++ govproject-backend/main.py
@@ -17,6 +17,8 @@ app = FastAPI(title="GovPreneurs API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
